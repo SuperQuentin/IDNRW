f00080ab29cb3a2f60aafb177dd7543d9a058c92
Quentin Aellen <quentin.aellen@cpnv.ch>
2021-04-30 13:07:27 +0200

    init project react-native ts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { t } from 'react-native-tailwindcss';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={t.textBlue500}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
