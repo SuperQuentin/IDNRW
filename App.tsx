dfd2449b087c261c9bc01d192bbc0bbe6d29e87e
Quentin Aellen <quentin.aellen@cpnv.ch>
2021-05-07 10:27:24 +0200

    it work

--- App.tsx
+++ App.tsx
@@ -1,22 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { t } from 'react-native-tailwindcss';
+import { StatusBar } from "expo-status-bar";
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={t.textBlue500}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.nav}>
+          <Text style={styles.button}>Bouton</Text>
+          <Text style={styles.button}>Bouton</Text>
+          <Text style={styles.button}>Bouton</Text>
+        </View>
+
+        <View style={styles.main}></View>
+
+        <View style={styles.nav}>
+          <Text style={[styles.button, styles.fatButton]}>Bouton</Text>
+          <Text style={styles.button}>Bouton</Text>
+          <Text style={styles.button}>Bouton</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+  },
+  main: {
+    flex: 1,
+  },
+  nav: {
+    flex: 1,
+    maxHeight: 64,
+    flexWrap: "nowrap",
+    flexShrink: 1,
+    flexDirection: "row",
+  },
+  button: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "coral",
+  },
+  fatButton: {
+    flex: 2,
   },
 });
