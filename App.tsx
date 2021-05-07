4861e4572245719d501976d2aed36d8f7cf29302
Quentin Aellen <quentin.aellen@cpnv.ch>
2021-05-07 10:00:51 +0200

    cbo

--- App.tsx
+++ App.tsx
@@ -1,22 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { t } from 'react-native-tailwindcss';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={t.textBlue500}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.item}>test</View>
+      <View style={styles.item}>test</View>
+      <View style={styles.item}>test</View>
+      <View style={styles.item}>test</View>
+      <View style={styles.item}>test</View>
+      <View style={styles.item}>test</View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    maxWidth: 128,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 8,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  item: {
+    width: 100,
+    maxHeight: 32,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 6,
+    marginBottom: 6,
+    padding: 4,
+    backgroundColor: "red",
   },
 });
