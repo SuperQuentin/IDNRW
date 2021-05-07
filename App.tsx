1e80d5c5b2da3ddd5f47c500688c48ebacd99e00
SuperQuentin <quentin.aellen@cpnv.ch>
2021-05-07 21:19:20 +0200

    refactor App function to class and clear default images

--- App.tsx
+++ App.tsx
@@ -1,22 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { t } from 'react-native-tailwindcss';
+import React, { Component } from 'react';
+import {Text, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={t.textBlue500}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Bonjour</Text>
+            </View>
+        )
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
