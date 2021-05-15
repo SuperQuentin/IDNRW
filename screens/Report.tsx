d055ff7f68172355ae93715e35260c2882d33e7f
SuperQuentin <quentin.aellen@cpnv.ch>
2021-05-15 18:37:56 +0200

    correct params list import

--- screens/Report.tsx
+++ screens/Report.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { StackScreenProps } from "@react-navigation/stack"
-import { RootParamList } from '../navigation/RootStackNavigator'
+import { ActionBottomTabParamList } from '../navigation/ActionBottomTabNavigator'
 import { View } from 'react-native'
 
-type ReportProps = StackScreenProps<RootParamList, 'Report'>
+type ReportProps = StackScreenProps< ActionBottomTabParamList, 'Report'>
 
 export default class Report extends Component<ReportProps>{
     render(){
