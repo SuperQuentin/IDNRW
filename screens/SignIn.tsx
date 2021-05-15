c206fea4dc3a8cd677eaded3e0e27ff5ba486533
SuperQuentin <quentin.aellen@cpnv.ch>
2021-05-15 18:37:34 +0200

    rename Login screen to SignIn

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -3,7 +3,7 @@ import { View, Text, Button } from "react-native";
 import { StackScreenProps } from "@react-navigation/stack"
 import { RootParamList } from '../navigation/RootStackNavigator'
 
-type LoginProps = StackScreenProps<RootParamList, 'Login'>
+type LoginProps = StackScreenProps<RootParamList, 'SignIn'>
 
 export default class Login extends Component<LoginProps> {
     constructor({ navigation, route }: LoginProps) {
