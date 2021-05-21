d6989dab48b09cfa839829605940d7fd22249e61
quentin.aellen <quentin.aellen@cpnv.ch>
2021-05-21 12:11:50 +0200

    add cities loading from api

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { View, Text, Button } from "react-native";
 import { StackScreenProps } from "@react-navigation/stack";
 import { RootParamList } from "../navigation/RootStackNavigator";
+import SquareButtonContainer from "../components/button/SquareButtonContainer";
+import SquareButton from "../components/button/SquareButton";
 
 import Input from "../components/form/Input";
 
@@ -10,18 +13,35 @@ export type SignInProps = StackScreenProps<RootParamList, "SignIn">;
 export default class SignIn extends Component<SignInProps> {
   constructor(props: SignInProps) {
     super(props);
+    this.state = {
+      cities: [],
+    };
+    this.getCities();
   }
 
   onPressConnection = () => {
     this.props.navigation.navigate("Action");
   };
 
+  getCities = async () => {
+    let response = await axios.get("http://127.0.0.1:8000/api/bases");
+    this.setState({ cities: response.data });
+    console.log(this.state.cities);
+  };
   render() {
     const { initials } = this.props.route.params;
 
     return (
       <View>
         <Input label="Initials" />
+        <SquareButtonContainer>
+          {this.state.cities &&
+            this.state.cities.map((city: any) => {
+              return (
+                <SquareButton key={city.id} label={city.name} value={city.id} />
+              );
+            })}
+        </SquareButtonContainer>
         <Button title="Connexion" onPress={this.onPressConnection} />
       </View>
     );
