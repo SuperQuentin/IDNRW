baab34aa4260e1f3941bb2182432b52f6dc12ee7
quentin.aellen <quentin.aellen@cpnv.ch>
2021-05-21 12:11:32 +0200

    create square button

--- components/button/SquareButton.tsx
+++ components/button/SquareButton.tsx
@@ -1,9 +1,39 @@
 import React, { Component } from "react";
+import { TouchableOpacity, ImageBackground, Text } from "react-native";
+import { TapGestureHandler } from "react-native-gesture-handler";
 
 export interface SquareButtonProps {
   label: string;
   value: string;
+  callback?: Function;
   img?: string;
 }
 
-export default class SquareButtonContainer extends Component<SquareButtonProps> {}
+export default class SquareButtonContainer extends Component<SquareButtonProps> {
+  constructor(props: SquareButtonProps) {
+    super(props);
+  }
+
+  onPressBtn = () => {
+    if (this.props.callback) {
+      this.props.callback({ city: this.props.value });
+    }
+  };
+
+  render() {
+    return (
+      <TouchableOpacity onPress={() => this.onPressBtn()}>
+        <ImageBackground
+          source={{ uri: this.props.img }}
+          style={{
+            width: 104,
+            height: 104,
+            backgroundColor: "#EE6C4D",
+          }}
+        >
+          <Text>{this.props.label}</Text>
+        </ImageBackground>
+      </TouchableOpacity>
+    );
+  }
+}
--- components/button/SquareButtonContainer.tsx
+++ components/button/SquareButtonContainer.tsx
@@ -1,17 +1,49 @@
-import React, { Component } from "react";
-import { View } from "react-native";
+import React, { Component, ReactNode } from "react";
+import { View, ScrollView, Text } from "react-native";
 import SquareButton from "./SquareButton";
 
 export interface SquareButtonContainerProps {
-  children: SquareButton;
+  children: ReactNode;
 }
 
 export default class SquareButtonContainer extends Component<SquareButtonContainerProps> {
   constructor(props: SquareButtonContainerProps) {
     super(props);
+    this.state = {
+      selectedLocation: null,
+    };
   }
 
+  handleCallback = (data: any) => {
+    this.setState({ selectedLocation: data.city });
+  };
+
   render() {
-    return <View>{this.props.children}</View>;
+    const childrenWithProps = React.Children.map(
+      this.props.children,
+      (child) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, {
+            callback: this.handleCallback,
+          });
+        }
+        return child;
+      }
+    );
+    return (
+      <View>
+        <Text>Ville selectionné : {this.state.selectedLocation}</Text>
+        <ScrollView
+          horizontal={true}
+          contentContainerStyle={{
+            flex: 1,
+            justifyContent: "space-between",
+          }}
+          style={{ width: 312, height: 208, margin: "auto" }}
+        >
+          {childrenWithProps}
+        </ScrollView>
+      </View>
+    );
   }
 }
