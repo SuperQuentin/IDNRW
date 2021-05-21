6c7a81f10717150f18b04062aa105ec4c006fa57
SuperQuentin <quentin.aellen@cpnv.ch>
2021-05-21 09:07:21 +0200

    fix text rendering error

--- screens/Drug.tsx
+++ screens/Drug.tsx
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import { StackScreenProps } from "@react-navigation/stack";
 import { ActionBottomTabParamList } from "../navigation/ActionBottomTabNavigator";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export type DrugProps = StackScreenProps<ActionBottomTabParamList, "Drug">;
 
 export default class Drug extends Component<DrugProps> {
-  constructor({ navigation, route }: DrugProps) {
-    super({ navigation, route });
+  constructor(props: DrugProps) {
+    super(props);
   }
 
   render() {
-    return <View>Drugs</View>;
+    return (
+      <View>
+        <Text>Drugs</Text>
+      </View>
+    );
   }
 }
--- screens/Report.tsx
+++ screens/Report.tsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { StackScreenProps } from "@react-navigation/stack";
 import { ActionBottomTabParamList } from "../navigation/ActionBottomTabNavigator";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export type ReportProps = StackScreenProps<ActionBottomTabParamList, "Report">;
 
 export default class Report extends Component<ReportProps> {
   render() {
-    return <View>Report</View>;
+    return (
+      <View>
+        <Text>Report</Text>
+      </View>
+    );
   }
 }
