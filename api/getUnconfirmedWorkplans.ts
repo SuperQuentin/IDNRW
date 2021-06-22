0d0d80b845138397b52b7c5b52cc2035d1b20581
quentin.aellen <quentin.aellen@cpnv.ch>
2021-06-22 10:08:58 +0200

    add api call to get Unconfirmed Workplans

---
+++ api/getUnconfirmedWorkplans.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { config } from "./config";
+
+const endpoint = `unconfirmedworkplans/`;
+
+export default async (token: string | null, id: number) => {
+  if (token === null) {
+    return false;
+  }
+  const axios_config = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  let response = await axios.get(config.url_base + endpoint, axios_config);
+  return response;
+};
